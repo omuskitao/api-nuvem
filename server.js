9e193ab8975fdb8832ee8a463110fd60058c7b3a
omuskitao <166729363+omuskitao@users.noreply.github.com>
2026-03-12 11:53:42 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+onst express = require ("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get("/", (req, res)=> {
+    res.json ({
+        mensagem: "API funcionando na NUVEM",
+        status: "online"
+    });
+});
+
+app.listen (PORT, () =>{
+    console.log (`Servidor rodando na porta ${PORT}`);
+});
